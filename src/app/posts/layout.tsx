1f6abd9069ddde33043b703e6bbcdccfd8c8c2ce
Trần Thị Minh Huyền <huyenttm@cozwork.com>
2023-06-26 23:11:33 +0700

    delete post

--- src/app/posts/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Layout = () => {
-  return (
-    <div>Navigate to Top</div>
-  )
-}
-
-export default Layout
--- src/app/posts/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Page = () => {
-  return (
-    <div>POSTS</div>
-  )
-}
-
-export default Page
